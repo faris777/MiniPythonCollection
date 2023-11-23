97891da18cf66312aec7214b1d850db4559adbf6
faris777 <fariszking@gmail.com>
2023-11-23 23:49:01 +0100

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,7 +1,7 @@
 def isPrime(n):
     result = False
     count = 0
-    for i in range(1,n//2+1):
+    for i in range(1,int(n**0.5) + 1):
         if n%i == 0:
             count +=1
     if count < 2:
